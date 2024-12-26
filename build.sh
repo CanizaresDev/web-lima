5ecd4a28e2eae9cfe297169e412925a8c458fe53
CanizaresDev <alberto2k7y@gmail.com>
2024-12-26 14:12:52 +0100

    CORS added

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-source env/Scripts/activate
+source .venv/Scripts/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-reflex init
-reflex export --frontend-only
 rm -rf public
+reflex init
+API_URL=https://web-lima.up.railway.app reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
--- rxconfig.py
+++ rxconfig.py
@@ -2,4 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="web",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://web-lima.up.railway.app"
+
+    ]
 )
